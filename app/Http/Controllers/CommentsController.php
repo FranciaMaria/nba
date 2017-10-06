b4f38c9d365bc481423768a6ce39a433b7a06133
Maria <mariafrancia88@gmail.com>
2017-10-06 15:13:25 +0200

    task 7

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -13,6 +13,7 @@ class CommentsController extends Controller
 	public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('words', ['only' => 'store']);
     }
 
     public function store(Team $team){
---
+++ resources/views/auth/forbidden-rude-words.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2>Forbidden for rude words: hate, idiot and stupid.</h2>
+
+    <hr>
+
+	<p>Back to <a href="/"> teams form. </a></p>
+</body>
+</html>
+
